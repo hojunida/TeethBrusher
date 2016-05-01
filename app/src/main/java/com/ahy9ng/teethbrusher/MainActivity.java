b679f77f578662b9121c56615cc1530a7730da5d
ahy9ng <ahy9ng@virginia.edu>
2016-05-01 14:20:59 -0400

    mean + stdev

--- app/src/main/java/com/ahy9ng/teethbrusher/MainActivity.java
+++ app/src/main/java/com/ahy9ng/teethbrusher/MainActivity.java
@@ -1,4 +1,5 @@
 package com.ahy9ng.teethbrusher;
+
 import android.app.Activity;
 import android.content.Context;
 import android.hardware.Sensor;
@@ -6,11 +7,19 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.util.Log;
 
 import java.io.FileOutputStream;
 import java.util.ArrayList;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
 public class MainActivity extends Activity implements SensorEventListener{
     public boolean init = false;
@@ -66,10 +75,38 @@ public class MainActivity extends Activity implements SensorEventListener{
 
     }
 
+    public double getMean(ArrayList<Double> data)
+    {
+        double sum = 0.0;
+        for(double a : data)
+            sum += a;
+        return sum/data.size();
+    }
+
+    public double getVariance(ArrayList<Double> data)
+    {
+        double mean = getMean(data);
+        double temp = 0;
+        for(double a :data)
+            temp += (mean-a)*(mean-a);
+        return temp/data.size();
+    }
+
+    public double getStdDev(ArrayList<Double> data)
+    {
+        return Math.sqrt(getVariance(data));
+    }
+
     public void classify(ArrayList<Double> x,ArrayList<Double> y,ArrayList<Double> z) {
-        Log.e("", getApplicationContext().getFilesDir().getAbsolutePath());
+        double mean_x = getMean(x);
+        double std_x = getStdDev(x);
+        double mean_y = getMean(y);
+        double std_y = getStdDev(y);
+        double mean_z = getMean(z);
+        double std_z = getStdDev(z);
+        //Log.e("", (mean_x + " " + std_x + " " + mean_y + " " + std_y + " " + mean_z + " " + std_z));
         try {
-            outputStream.write((x.toString() + " " + y.toString() + " " + z.toString()).getBytes());
+            outputStream.write((mean_x + " " + std_x + " " + mean_y + " " + std_y + " " + mean_z + " " + std_z).getBytes());
         } catch (Exception e){
             e.printStackTrace();
         }
@@ -79,5 +116,18 @@ public class MainActivity extends Activity implements SensorEventListener{
 //        Log.e("",z.toString());
     }
 
-
+        public void getInternalTextFileString(){
+                try {
+                        FileInputStream inStream = this.openFileInput("myStringFile");
+                        InputStreamReader inputStreamReader = new InputStreamReader(inStream);
+                        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                        StringBuilder finalString = new StringBuilder(); String oneLine;
+                        while ((oneLine = bufferedReader.readLine()) != null) {
+                                finalString.append(oneLine);
+                            }
+                        bufferedReader. close();
+                        inStream. close();
+                        inputStreamReader. close();
+                        //textView. setVisibility(View.VISIBLE); textView.setText(finalString.toString());
+                        } catch (Exception e) { e.printStackTrace(); } }
 }
